9a5cad4c46a4093895d5f0d979f2a93202c8a678
Vinayak Ghosh <r.vinayakghosh@gmail.com>
2026-07-07 01:24:32 +0530

    created the agent.py file

--- 4_Stateful_LLM_with_RAG/agent.py
+++ 4_Stateful_LLM_with_RAG/agent.py
@@ -22,3 +22,44 @@ available_tools = {
 # Set a strict guardrail against infinite agent loops
 MAX_ITERATIONS = 5
 iterations = 0
+
+
+while iterations < MAX_ITERATIONS:
+    iterations += 1
+    user_input = input("You: ")
+
+    if user_input.lower() in ["exit", "quit", "bye"]:
+        break
+
+    message_list.append({"role": "user", "content": user_input})
+
+    while True:
+
+        response = client.responses.create(
+            model=model,
+            tools=tools,
+            input=message_list
+        )
+
+        message_list += response.output
+
+        tool_called = False 
+        for item in response.output:
+            if item.type=='function_call':
+                tool_called=True
+
+                # Function call logic for every function call
+                result = execute_tool(item, available_tools)
+
+                # Provide function call results to model
+                message_list.append({
+                    "type": "function_call_output",
+                    "call_id": item.call_id,
+                    "output": result
+                })
+        if not tool_called:
+            break
+
+    assistant_response = response.output_text
+    print("Assistant: ", assistant_response)
+    print()
